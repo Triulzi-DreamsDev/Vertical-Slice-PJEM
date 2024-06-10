21b54ed6ddf8222409e64fc0f928926e57e6bd8b
dreamingery <g_triulzi@hotmail.com>
2024-06-10 12:11:24 -0600

    Merge branch 'Ruben'

--- Assets/Lvl 1/Scripts/Dialogue System with Choices/Quest System/DialogueManager.cs
+++ Assets/Lvl 1/Scripts/Dialogue System with Choices/Quest System/DialogueManager.cs
@@ -10,7 +10,7 @@ using System;
 public class DialogueManager : GameCTRL
 {
     [Header("Params")]
-    [SerializeField] private float typingSpeed = 0.01f;
+    [SerializeField] private float typingSpeed = 0.03f;
 
     [Header("Load Globals JSON")]
     [SerializeField] private TextAsset loadGlobalsJSON;

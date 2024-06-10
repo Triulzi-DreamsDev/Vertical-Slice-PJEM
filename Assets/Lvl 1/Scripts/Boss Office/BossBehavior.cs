bfb494cb859255f40a6b5ad9b71d56bf3ebd0312
Ruben7989 <rubengodinezdesarrolladorti@gmail.com>
2024-06-10 13:20:43 -0600

    Vistas y cambios Boss
    Se modificó el código para que se vea al hablar al jugador y se acomodó la vista para que, al dirigirse hacia la puerta, pueda detectarlo sin conflictos.

--- Assets/Lvl 1/Scripts/Boss Office/BossBehavior.cs
+++ Assets/Lvl 1/Scripts/Boss Office/BossBehavior.cs
@@ -53,7 +53,8 @@ public class BossBehavior : GameCTRL
 
         if (DialogueManager.GetInstance().dialogueIsPlaying && (questState == 0 || questState == 1))
         {
-            //transform.LookAt(player);
+            // Asegúrate de que el jefe mire al jugador mientras habla
+            transform.LookAt(player.position);
             animator.SetBool("idle", false);
             animator.SetBool("walking", false);
             animator.SetBool("angry", false);
@@ -68,47 +69,25 @@ public class BossBehavior : GameCTRL
             if (steps == 0)
             {
                 ApproachPlayer();
-
             }
-
         }
+
         if (questState == 1)
         {
             goBackOffice = true;
         }
+
         if (goBackOffice)
         {
             steps = 1;
-            
-
-            /*if (Vector3.Distance(transform.position, new Vector3(9f, 3.7f, 1.12f)) >= .5 && centro == false)
-            {
-                transform.LookAt(new Vector3(9f, 3.7f, 1.12f));
-                transform.position += transform.forward * 2f * Time.deltaTime;
-
-
-
-                animator.SetBool("idle", false);
-                animator.SetBool("walking", true);
-                animator.SetBool("angry", false);
-                animator.SetBool("pointing", false);
-                animator.SetBool("talking", false);
-            } 
-            if (Vector3.Distance(transform.position, new Vector3(9f, 2f, 1.12f)) <= 2)
-            {
-                centro = true;
-            }
-            */
 
             if (Vector3.Distance(transform.position, new Vector3(25f, 3.7f, 2.65f)) >= .5)
             {
-                /*transform.LookAt(new Vector3(25f, 3.5f, 2.65f));
-                transform.position += transform.forward * 2f * Time.deltaTime;*/
                 Vector3 targetVector = DestinationPuerta.transform.position;
-                //transform.position += transform.forward * 2f * Time.deltaTime;
-                agent.SetDestination(targetVector);
-                transform.LookAt(targetVector);
+                Vector3 lookAtPosition = new Vector3(targetVector.x, transform.position.y, targetVector.z); // Mantiene la misma altura
+                transform.LookAt(lookAtPosition);
 
+                agent.SetDestination(targetVector);
 
                 animator.SetBool("idle", false);
                 animator.SetBool("walking", true);
@@ -120,7 +99,6 @@ public class BossBehavior : GameCTRL
 
         if ((questState == 2 || questState == 4) && cajon == false)
         {
-
             transform.LookAt(new Vector3(-5f, 1.6f, 5.38f));
             if (Vector3.Distance(transform.position, new Vector3(-5.09f, 1.53f, 5.5f)) >= .2)
             {
@@ -132,9 +110,8 @@ public class BossBehavior : GameCTRL
                 animator.SetBool("pointing", false);
                 animator.SetBool("talking", false);
             }
-
-
         }
+
         if (cajon)
         {
             transform.LookAt(player);
@@ -150,15 +127,11 @@ public class BossBehavior : GameCTRL
         rb.velocity += additionalVelocity;
     }
 
-
-
     void ApproachPlayer()
     {
-        //var fwd1= Vector3.Dot(rb.velocity, tr.forward);
         if (Vector3.Distance(transform.position, player.position) > distanciaDelPlayer)
         {
             Vector3 targetVector = player.transform.position;
-            //transform.position += transform.forward * 2f * Time.deltaTime;
             agent.SetDestination(targetVector);
             transform.LookAt(targetVector);
 
@@ -167,7 +140,8 @@ public class BossBehavior : GameCTRL
             animator.SetBool("angry", false);
             animator.SetBool("pointing", false);
             animator.SetBool("talking", false);
-        } else if (Vector3.Distance(transform.position, player.position) <= distanciaDelPlayer)
+        }
+        else if (Vector3.Distance(transform.position, player.position) <= distanciaDelPlayer)
         {
             animator.SetBool("idle", true);
             animator.SetBool("walking", false);
@@ -175,9 +149,5 @@ public class BossBehavior : GameCTRL
             animator.SetBool("pointing", false);
             animator.SetBool("talking", false);
         }
-
     }
-
-
-
 }

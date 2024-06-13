0347a3ca65f13bb3a60d00a2f15e86f3a118fef4
Ruben7989 <rubengodinezdesarrolladorti@gmail.com>
2024-06-13 17:21:39 -0600

    Scripts Spawns
    Se crearon los scripts de las escenas Archivos Nuevos y Archivos Viejos para poder utilizar el player Prefabs y que tengan sus respectivas apariciones.

---
+++ Assets/Lvl 1/Scripts/Main CTRLs/Spawn_AN.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_AN : GameCTRL
+{
+    [SerializeField]
+    Transform initialLobbySpawn;
+    [SerializeField]
+    Transform archivosNuevosSpawn;
+
+    [SerializeField]
+    GameObject playerPrefab;
+
+    bool spawn = false;
+
+    void Awake()
+    {
+        if (rooms == 1) // Archivos Nuevos
+        {
+            GameObject player = Instantiate(playerPrefab, archivosNuevosSpawn.position, archivosNuevosSpawn.rotation);
+        }
+        else if (rooms == 0) // Lobby
+        {
+            GameObject player = Instantiate(playerPrefab, initialLobbySpawn.position, initialLobbySpawn.rotation);
+        }
+    }
+
+    void Update()
+    {
+    }
+}
---
+++ Assets/Lvl 1/Scripts/Main CTRLs/Spawn_AV.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_AV : GameCTRL
+{
+    [SerializeField]
+    Transform initialLobbySpawn;
+    [SerializeField]
+    Transform archivosViejosSpawn;
+
+    [SerializeField]
+    GameObject playerPrefab;
+
+    bool spawn = false;
+
+    void Awake()
+    {
+        if (rooms == 2) // Archivos Viejos
+        {
+            GameObject player = Instantiate(playerPrefab, archivosViejosSpawn.position, archivosViejosSpawn.rotation);
+        }
+        else if (rooms == 0) // Lobby
+        {
+            GameObject player = Instantiate(playerPrefab, initialLobbySpawn.position, initialLobbySpawn.rotation);
+        }
+    }
+
+    void Update()
+    {
+    }
+}
